f97936ae045fe97cac3d46e28ff391f5686c7b4c
phitup <tranquangphi260997@gmail.com>
2018-03-25 06:18:06 -0700

    connection SOCKET.IO

--- ChatRealTime/App.js
+++ ChatRealTime/App.js
@@ -5,26 +5,22 @@ import {
   Text,
   View
 } from 'react-native';
+import io from 'socket.io-client/dist/socket.io.js';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.socket = io("http://192.168.1.2:3000" , {jsonp:false});
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
         </Text>
       </View>
     );
---
+++ SERVER/Server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+io.on("connection" , function(socket){
+    console.log("co nguoi vua ket noi" + socket.id);
+});
